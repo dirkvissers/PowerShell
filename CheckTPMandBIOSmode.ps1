0a3db5274852dcb342066cbe4d9724c8647b1c7d
dirkvissers <72601149+dirkvissers@users.noreply.github.com>
2021-12-20 21:29:42 +0100

    Checking TPM and BIOS mode
    If you have a big list of devices and want to know which devices have a TPM present and in which mode Windows has been installed.
    Generates msinfo32 output to C:\Temp\testmsinfo32.nfo and generates output to the console for reporting purposes.

---
+++ CheckTPMandBIOSmode.ps1
@@ -0,0 +1,30 @@
+﻿If(((Get-Tpm).TpmPresent -eq $true)-and((Get-Tpm).TpmEnabled) -eq $true){Write-Host "TPM present and enabled."}
+If(((Get-Tpm).TpmPresent -eq $true)-and((Get-Tpm).TpmEnabled) -eq $false){Write-Host "TPM present, but disabled."}
+If(((Get-Tpm).TpmPresent -eq $true)-and((Get-Tpm).TpmEnabled) -eq $null){Write-Host "TPM present, but Enabled property missing."}
+If(((Get-Tpm).TpmPresent -eq $false)-or((Get-Tpm).TpmPresent -eq $null)){Write-Host "TPM Missing."}
+
+if (Test-Path $env:windir\Panther\setupact.log) {(Select-String 'Detected boot environment' -Path "$env:windir\Panther\setupact.log"  -AllMatches).line -replace '.*:\s+'} else {if (Test-Path HKLM:\System\CurrentControlSet\control\SecureBoot\State) {"Installed in UEFI mode."} else {"Installed in Legacy mode."}}
+
+# Create new folder "C:\Temp"
+If( (Test-Path C:\Temp) -eq $True){
+$Path = Get-ChildItem C:\ -Hidden | Where-Object {$_.Name -eq "Temp"}
+}
+
+Else{
+$Path = New-Item -Path "C:\" -Name "Temp" -ItemType "Directory"
+}
+
+If((Test-Path C:\temp\testmsinfo32.nfo) -eq $false){msinfo32 /nfo C:\temp\testmsinfo32.nfo}
+
+[bool]$Run=$true
+While($Run -eq $true){
+if((Test-Path C:\temp\testmsinfo32.nfo) -eq $true ){
+    sleep 450
+    $Values = Get-Content C:\temp\testmsinfo32.nfo
+    $Values | Select-String "BIOS-modus" -Context 0,1
+    $Values | Select-String "Status beveiligd opstarten"
+    $Values | Select-String "BIOS mode" -Context 0,1
+    $Values | Select-String "Secure Boot State" -Context 0,1
+    [bool]$Run=$false
+    Remove-Item C:\temp\testmsinfo32.nfo -Force}
+}
