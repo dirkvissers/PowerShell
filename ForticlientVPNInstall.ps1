a3dfc1961c22a3baafad7ba565d48e4cffb6fb79
dirkvissers <72601149+dirkvissers@users.noreply.github.com>
2021-12-20 21:54:09 +0100

    Install Forticlient VPN Only agent
    Downloads the newest agent and installs this silently without reboot.
    Once Fortinet changes the download link to the MSI, this should work properly.
    
    Doesn't contain a profile, if you want a profile please investigate FortiConfigurator and sign up for Fortinet Dev at your Fortinet Dealer.
    You would have to host you MSI containing the profile and change the URL in the Invoke-WebRequest cmdlet.

---
+++ ForticlientVPNInstall.ps1
@@ -0,0 +1,23 @@
+﻿<#
+
+https://links.fortinet.com/forticlient/win/vpnagent
+
+#>
+
+
+$LogoDest = "C:\Temp\FortiClientVPNOnlineInstaller.exe" 
+$LogoDestPath = "C:\Temp"
+	
+# Create new folder "C:\Temp" for logo placement
+If ((Test-Path $LogoDest) -ne $True)
+	{
+		If ((Test-Path $LogoDestPath) -ne $True) { $Path = New-Item -Path "C:\" -Name "Temp" -ItemType "Directory" }
+		Invoke-WebRequest -Uri "https://links.fortinet.com/forticlient/win/vpnagent" -OutFile $LogoDest
+	}
+
+Start-Process -FilePath "C:\Temp\FortiClientVPNOnlineInstaller.exe /quiet /norestart" -ArgumentList "/quiet","/norestart"
+
+
+"C:\Temp\FortiClientVPNOnlineInstaller.exe" FortiClientVPN.exe /quiet /norestart
+
+Start-Process Msiexec.exe -Wait -ArgumentList '/i C:\temp\FortiClientVPN.msi REBOOT=ReallySuppress /qn'
